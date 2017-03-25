44d9de8b32207210a44c4a00256254bda273b18c
danielle-w233 <dwhitfi@uwo.ca>
2017-03-24 23:02:35 -0400

    Update
    Edited settings.js

--- 9BitTestRepo/src/main/js/settings.js
+++ 9BitTestRepo/src/main/js/settings.js
@@ -1,3 +1,6 @@
+//settings.js
+//No recorded activity
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -51,7 +54,7 @@ var SettingsComponent = React.createClass({
         let message = "";
 
         //check if category is selected
-        if ( (!this.state.new_username) ||(!this.state.new_password) ){
+        if (!((this.state.new_username) || (this.state.new_password))){
             message += "Please change a field. ";
         }
 
@@ -59,24 +62,67 @@ var SettingsComponent = React.createClass({
         //otherwise display an error messsage and stay on the same page
         if (message == ""){
             let new_username = this.state.new_username;
-            let category = this.state.new_password;
-            let old_password = this.state.old_password;
-            fetch('http://localhost:8080/userAccount/addTransaction?'
-                + 'new_username=' + new_username + '&category=' + category + '&old_password=' + old_password, {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json"
+            let new_password = this.state.new_password;
+            let pass = 0
+            let user = 0
+            if(new_username) {
+                fetch('http://localhost:8080/settings/setUsername?'
+                    + 'username=' + new_username, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }).then(res => {
+                    if (res.ok) {
+                    this.setState({user: 1});
                 }
-            }).then(res =>{
-                if(res.ok){
-                    this.setState({error: "Transaction added!"});
-                }
-                else{
-                    this.setState({error: "Transaction couldn't be added."});
+            else {
+                    this.setState({user: 2});
                 }
             });
+            }
+            if(new_password){
+                fetch('http://localhost:8080/settings/setPassword?'
+                    + 'password=' + new_password, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }).then(res => {
+                    if (res.ok) {
+                    this.setState({pass: 1});
+                }
+            else {
+                    this.setState({pass: 2});
+                }
+            });
+            }
+            if(user == 0){
+                if(pass == 1){
+                    this.setState({user_error: "Password updated"})
+                }
+                if(pass == 2){
+                    this.setState({user_error: "Passwordn not updated"})
+                }
+            }
+            else if(user == 1){
+                if(pass == 1){
+                    this.setState({user_error: "Username and password updated"})
+                }
+                if(pass == 2){
+                    this.setState({user_error: "Username updated, password not updated."})
+                }
+            }
+            else if(user == 2){
+                if(pass == 1){
+                    this.setState({user_error: "Username not updated, password updated."})
+                }
+                if(pass == 2){
+                    this.setState({user_error: "Username and password not updated."})
+                }
+            }
         } else {
-            this.setState({error: message});
+            this.setState({user_error: message});
         }
     },
 
@@ -84,18 +130,18 @@ var SettingsComponent = React.createClass({
         // Prevents reinitialization
         e.preventDefault();
 
-        let message = "";
+        let income_message = "";
 
         //check if category is selected
         if (!this.state.new_income){
-            message += "Please enter your income. ";
+            income_message += "Please enter your income.";
         }
 
         //if no invalid inputs, then proceed to change income
         //otherwise display an error messsage and stay on the same page
-        if (message == ""){
+        if (income_message == ""){
             let new_income = this.state.new_income;
-            fetch('http://localhost:8080/userAccount/addTransaction?'
+            fetch('http://localhost:8080/settings/addTransaction?'
                 + 'new_income=' + new_income, {
                 method: 'POST',
                 headers: {
@@ -103,29 +149,29 @@ var SettingsComponent = React.createClass({
                 }
             }).then(res =>{
                 if(res.ok){
-                    this.setState({error: "Income updated!"});
-                }
-                else{
-                    this.setState({error: "Income couldn't be updated."});
-                }
-            });
+                this.setState({income_error: "Income updated!"});
+            }
+        else{
+                this.setState({income_error: "Income couldn't be updated."});
+            }
+        });
         } else {
-            this.setState({error: message});
+            this.setState({income_error: income_message});
         }
     },
 
     handleAchievements(e){
         fetch('http://localhost:8080/transaction/achievement1?'
-           , {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then(res =>{
+            , {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(res =>{
             if(res.ok){
-                document.getElementById("1transaction").src = "http://imgur.com/yp98Nwz.jpg";
-            }
-        });
+            document.getElementById("1transaction").src = "http://imgur.com/yp98Nwz.jpg";
+        }
+    });
 
         fetch('http://localhost:8080/transaction/achievement5?'
             , {
@@ -135,93 +181,35 @@ var SettingsComponent = React.createClass({
                 }
             }).then(res =>{
             if(res.ok){
-                document.getElementById("5transactions").src = "http://imgur.com/rfcYAkY.jpg";
-            }
-        });
+            document.getElementById("5transactions").src = "http://imgur.com/rfcYAkY.jpg";
+        }
+    });
     },
 
 
     render () {
         return (
             <body style={{margin:0, padding:0, fontSize:0}} onLoad={this.handleAchievements}>
-            <title>My Account</title>
+                <title>My Account</title>
 
 
-            {/* wrapper for SETTINGS page*/}
-            <div style={{marginLeft:150, marginTop:20}}>
-                <font style={{fontFamily:"Arial"}}>
-                    <font size="+5">My Account</font>
-                    <div style={{marginTop:30}}>
+                {/* wrapper for SETTINGS page*/}
+                <div style={{marginLeft:150, marginTop:20}}>
+                    <font style={{fontFamily:"Arial"}}>
+                        <font size="+5">My Account</font>
+                        <div style={{marginTop:30}}>
 
-                        {/* three piddly achievements */}
-                        <div style = {{marginTop:30}}>
-                            <font size="+2">Achievements</font> <br/>
+                            {/* three piddly achievements */}
+                            <div style = {{marginTop:30}}>
+                                <font size="+2">Achievements</font> <br/>
                                 <img id="accountcreated" src="http://imgur.com/PhbcBaA.jpg" width="200" height="325" alt=""/>
                                 <img id="1transaction" src="http://imgur.com/LihzuRq.png" width="200" height="325" alt=""/>
                                 <img id="5transactions" src="http://imgur.com/b8yATih.png" width="200" height="325" alt=""/>
 
-                        </div>
-
-                        {/* budget form */}
-                        <div style={{marginTop:60}}>
-                            <font size="+2">Edit Budget</font>
-                            <form onSubmit={this.handleIncomeCheck}>
-                                <div style={{marginTop:30}}>
-                                    <font size="+2">New Income:</font>
-                                    <br/>
-                                </div>
-                                <font>$</font>
-                                <input type="number" min="0.01" step="0.01" max="10000"  ref="new_income" name="new_income" defaultValue={this.state.new_income} onChange={this.handleIncomeChange}/>
-                            </form>
-
-                            <div style={{marginTop:30}}>
-                                <input type="submit" value="Submit Income Change"/>
                             </div>
                         </div>
-
-                        {/* profile change form */}
-                        <div style={{marginTop:30}}>
-                            <form onSubmit={this.handleProfileCheck}>
-
-                                {/* new username input*/}
-                                <div style={{marginTop:30}}>
-                                    <font size="+2">New Username:</font>
-                                    <br/>
-                                </div>
-                                <input type="text" ref="new_username" name="new_username" defaultValue={this.state.new_username} onChange={this.handleUsernameChange}/>
-
-                                {/* new password input*/}
-                                <div style={{marginTop:20}}>
-                                    <font size="+2">New Password:</font>
-                                    <br/>
-                                </div>
-                                <input type="password" ref="new_username" name="new_username" defaultValue={this.state.new_username} onChange={this.handlePasswordChange}/>
-
-                                {/*/!* old_password selection *!/
-                                 <div style={{marginTop:40}}>
-                                 <font size="+2">Verify password:</font>
-                                 <br/>
-                                 </div>
-                                 <input ref="old_password" type="password" id="old_password" onChange={this.handleOldPasswordChange}/>*/}
-
-                                {/* submit profile changes button*/}
-                                <div style={{marginTop:30}}>
-                                    <input type="submit" value="Submit Profile Changes"/>
-                                </div>
-                            </form>
-                        </div>
-
-                        {/* error message display */}
-                        <div style={{marginTop:60}}>
-                            <font size="+1" color="red">
-                                <b>
-                                    {this.state.error}
-                                </b>
-                            </font>
-                        </div>
-                    </div>
-                </font>
-            </div>
+                    </font>
+                </div>
             </body>
         );
     }
